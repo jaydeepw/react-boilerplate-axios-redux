81bd88fa957a1d1a1c803d254f31f229d8bbd5f4
Jaydeep <jwagh@whileone.in>
2024-06-04 09:51:34 +0530

    add initial repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { blueGrey } from '@material-ui/core/colors';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import store, { history } from './store/configureStore';
+import { verifyToken } from './services/tokenService';
+import App from './containers/app/AppContainer';
+
+const mountNode = document.getElementById('root');
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: blueGrey,
+  },
+});
+
+// Used to log in if token is valid
+store.dispatch(verifyToken());
+
+ReactDOM.render(
+  <Suspense fallback={<div>Error! Please refresh the page</div>}>
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <App />
+        </ConnectedRouter>
+      </Provider>
+    </MuiThemeProvider>
+  </Suspense>,
+  mountNode
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
