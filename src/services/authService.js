b8d020f26b093138e269a521b048ce787d687f31
Jaydeep <jwagh@whileone.in>
2024-06-04 09:57:19 +0530

    Login with the local API is working

--- src/services/authService.js
+++ src/services/authService.js
@@ -8,7 +8,7 @@ import { setLocalStorage, clearLocalStorage } from '../utils/storageUtil';
 export const login = ({ email, password }) => {
   return (dispatch) => {
     axios
-      .post(API_URL + 'auth/login', { email, password })
+      .post(API_URL + 'auth/login', { username: email, password })
       .then((response) => {
         dispatch(loginSuccess(response.data.token));
         setLocalStorage(JWT_TOKEN, response.data.token);
