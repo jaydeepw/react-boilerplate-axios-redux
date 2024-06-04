645425ae9f02457dcb08a2ea9daeab018f67da22
Jaydeep <jwagh@whileone.in>
2024-06-04 11:32:28 +0530

    Change footer

--- src/components/common/footer/Footer.js
+++ src/components/common/footer/Footer.js
@@ -20,7 +20,7 @@ const Footer = (props) => {
   return (
     <div className={classes.legal}>
       <div className="copyright">
-        © 2016 - 2017 <a href="https://github.com/Bikranshu">Krishna Prasad Timilsina</a>
+        © 2024 <a href="https://github.com/jaydeepw">JaydeepW</a>
       </div>
     </div>
   );
--- src/containers/app/AppContainer.js
+++ src/containers/app/AppContainer.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
 // Import custom components
-import MainRouter from '../../routers/routes';
+import Routes from '../../routers/routes';
 
 class AppContainer extends Component {
   render() {
-    return <MainRouter />;
+    return <Routes />;
   }
 }
 
